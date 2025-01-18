005035e2ac167114922615b3ca2df75b145f1c25
Ar4sCode <ar4sazizi@gmail.com>
2025-01-19 02:12:53 +0330

    use Formik to handel form submission to add new ingredient after clicking on add button and the form will be reset

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,31 +1,52 @@
-import {useFormik} from "formik"
-import * as Yup from "yup"
+import { useFormik } from "formik";
+import { useState } from "react";
+import * as Yup from "yup";
 
 export const MainContent = () => {
+  const [ingredients, setIngredients] = useState([]);
+
   const formik = useFormik({
     initialValues: {
-      ingredient: ""
+      ingredient: "",
     },
     validationSchema: Yup.object({
-      ingredient: Yup.string()
-        .required("Please Enter an Ingredient")
+      ingredient: Yup.string().required("Please enter an ingredient"),
     }),
-    onSubmit: (values) => {
-      // create recipe
-    }
-  })
+    onSubmit: (values, { resetForm }) => {
+      setIngredients([...ingredients, values.ingredient]); // Add the ingredient
+      resetForm(); // Clear the input field
+    },
+  });
 
   return (
     <main>
+      <div>
       <form onSubmit={formik.handleSubmit}>
-        <input type="text" onChange={formik.handleChange} placeholder="e.g. oregano" />
-        <button type="submit">Add Ingredient</button>
-      </form>
+        <input
+          id="ingredient"
+          name="ingredient"
+          type="text"
+          value={formik.values.ingredient}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="e.g. oregano"
+        />
+        <button type="submit">+ Add Ingredient</button>
 
+      </form>
         {formik.touched.ingredient && formik.errors.ingredient ? (
           <div className="input-error">{formik.errors.ingredient}</div>
         ) : null}
+      </div>
 
+      <div className="container">
+        <h1>Ingredient on hand:</h1>
+        <ul>
+          {ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
+        </ul>
+      </div>
     </main>
-  )
-}
+  );
+};
